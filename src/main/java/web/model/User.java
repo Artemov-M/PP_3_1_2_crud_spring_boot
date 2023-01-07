43ba52d1199c42a415d2cd58aeb635cf06efc933
Mikhail Artemov <artemov.m.p@gmail.com>
2023-01-07 21:27:27 +0400

    Перенёс CRUD-приложение на Spring Boot.

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -2,7 +2,7 @@ package web.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
